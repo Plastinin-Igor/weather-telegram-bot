3d0221aba1d114e204011ca3a313b04de090215c
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-09-16 10:51:56 +0300

    Initial commit

---
+++ src/main/java/ru/plastinin/weather_telegram_bot/WeatherTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.plastinin.weather_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/plastinin/weather_telegram_bot/WeatherTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.plastinin.weather_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
